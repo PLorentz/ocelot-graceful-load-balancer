4b193f66a1cd3fe3deaa6fcb469f5632d9c66000
Pedro Lorentz <PLorentz@users.noreply.github.com>
2024-03-25 03:59:56 +0000

    Fixing extra slash messing with request URI in the DelegatingHandler.

--- src/Ocelot.GracefulLoadBalancer/GracefulLoadBalancerDelegatingHandler.cs
+++ src/Ocelot.GracefulLoadBalancer/GracefulLoadBalancerDelegatingHandler.cs
@@ -29,7 +29,7 @@ namespace Ocelot.LoadBalancer.LoadBalancers
                     await balancer.MarkAsBad(new ServiceHostAndPort(request.RequestUri.Host,request.RequestUri.Port,request.RequestUri.Scheme));
                     // Get a new lease from the balancer
                     var s = await balancer.Lease(null); /* No Http Context needed for the balancer */
-                    request.RequestUri = new Uri($"{s.Data.Scheme}://{s.Data.DownstreamHost}:{s.Data.DownstreamPort}/{request.RequestUri.PathAndQuery}");
+                    request.RequestUri = new Uri($"{s.Data.Scheme}://{s.Data.DownstreamHost}:{s.Data.DownstreamPort}{request.RequestUri.PathAndQuery}");
 
                     response = await base.SendAsync(request, cancellationToken);
                 }
